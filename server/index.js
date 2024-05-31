202027166450a5e236325a6a7905dbd3cca10105
Ankit kr Shrivastava <ankitshrivastavaksa@gmail.com>
2024-05-31 20:06:03 +0530

    new updated

--- server/index.js
+++ server/index.js
@@ -21,6 +21,9 @@ app.use(cors());
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
 
+app.get('/', (req, res) => {
+  res.send('products api running new deploy');
+});
 // theser r api routes
 app.use('/api', require('./routes/User.js'));
 app.use('/api', require('./routes/LoginRoute.js'));
@@ -32,6 +35,6 @@ app.use('/api', require('./routes/likes.js'));
 app.use('/api',require('./routes/GetLikes.js'));
 app.use('/api',require('./routes/getpost.js'))
 app.use('/api',require('./routes/Delete.js'))
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log('Server is listenin on PORT :' + PORT);
 });
