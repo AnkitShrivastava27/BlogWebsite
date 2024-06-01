6e95e03d99cd0f1369451c30afaa4d9974bf4039
Ankit kr Shrivastava <ankitshrivastavaksa@gmail.com>
2024-06-01 12:12:13 +0530

    new file

--- server/index.js
+++ server/index.js
@@ -3,40 +3,32 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
 const cors = require('cors');
-const CorsConfig ={
-  origin:"*",
-  credential: true,
-  methods: ["Post","Get","Put","Delete"],
 
+const CorsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["POST", "GET", "PUT", "DELETE"],
 }
 
-
 const port = process.env.PORT || 5000;
-const url2 = process.env.url  || "mongodb+srv://ankit:12ankit3@new.cq1ewgq.mongodb.net/"
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://ankit:12ankit3@new.cq1ewgq.mongodb.net/";
+
 // Middleware
-app.options("",cors(CorsConfig));
+app.options("*", cors(CorsConfig)); // Match any URL for CORS preflight requests
 app.use(cors(CorsConfig)); 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
 
+// Connect to MongoDB
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
-mongoose.connect(url2, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
-
-
-/*app.get('/', (req, res) => {
-  res.send('products api running new deploy');
-});*/
 // These are API routes
 app.use('/api', require('./routes/User.js'));
 app.use('/api', require('./routes/LoginRoute.js'));
@@ -46,8 +38,9 @@ app.use('/api', require('./routes/mypost.js'));
 app.use('/api', require('./routes/home.js'));
 app.use('/api', require('./routes/likes.js'));
 app.use('/api', require('./routes/GetLikes.js'));
-app.use('/api',require('./routes/getpost.js'))
-app.use('/api',require('./routes/Delete.js'))
+app.use('/api', require('./routes/getpost.js'));
+app.use('/api', require('./routes/Delete.js'));
+
 app.listen(port, () => {
-  console.log('Server is listenin on PORT :' + port);
+  console.log(`Server is listening on PORT: ${port}`);
 });
