0c8662b0c9f53c0a1eb698e89cb3a20802fcb234
Ankit kr Shrivastava <ankitshrivastavaksa@gmail.com>
2024-06-26 20:48:57 +0530

    original

--- server/routes/FirstPage.js
+++ server/routes/FirstPage.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('gg', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
      console.log("hello world");
      res.send('Hello World!');
