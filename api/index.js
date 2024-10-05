b9bf930bfeb42d2171a93976d618cb3b75508f21
Ankit kr Shrivastava <ankitshrivastavaksa@gmail.com>
2024-10-05 23:23:53 +0530

    indexchanged

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 // CORS configuration
 const corsOptions = {
-  origin: "https://wrightist-p6uxroxk2-ankitshrivastava27s-projects.vercel.app/", 
+  origin: "https://wrightist-p6uxroxk2-ankitshrivastava27s-projects.vercel.app", // Remove trailing slash
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 };
@@ -20,10 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 const mongoURI = process.env.MONGO_URI || "mongodb+srv://ankit:12ankit3@new.cq1ewgq.mongodb.net/new?retryWrites=true&w=majority&appName=new";
 
 // Connect to MongoDB
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true 
-})
+mongoose.connect(mongoURI)
 .then(() => {
   console.log('Connected to MongoDB');
 })
@@ -31,19 +28,19 @@ mongoose.connect(mongoURI, {
   console.error('Error connecting to MongoDB:', error);
 });
 
-
 // Routes
-app.use('/',require('../routes/FirstPage'));
-app.use('/api', require('../routes/User'));
-app.use('/api', require('../routes/LoginRoute'));
-app.use('/api', require('../routes/profile'));
-app.use('/api', require('../routes/newpost'));
-app.use('/api', require('../routes/mypost'));
-app.use('/api', require('../routes/home'));
-app.use('/api', require('../routes/likes'));
-app.use('/api', require('../routes/GetLikes'));
-app.use('/api', require('../routes/getpost'));
-app.use('/api', require('../routes/Delete'));
+// Make sure the path to routes is correct
+app.use('/', require('./routes/FirstPage')); // Adjusted path
+app.use('/api', require('./routes/User'));
+app.use('/api', require('./routes/LoginRoute'));
+app.use('/api', require('./routes/profile'));
+app.use('/api', require('./routes/newpost'));
+app.use('/api', require('./routes/mypost'));
+app.use('/api', require('./routes/home'));
+app.use('/api', require('./routes/likes'));
+app.use('/api', require('./routes/GetLikes'));
+app.use('/api', require('./routes/getpost'));
+app.use('/api', require('./routes/Delete'));
 
 // Start the server
 const port = process.env.PORT || 5000;
