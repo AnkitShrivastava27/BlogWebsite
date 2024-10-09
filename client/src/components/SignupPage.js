f124fe1ff47f676cc8abbeb6e2024ff19c30a7cd
Ankit kr Shrivastava <ankitshrivastavaksa@gmail.com>
2024-10-10 00:22:05 +0530

    2.0Released

--- client/src/components/SignupPage.js
+++ client/src/components/SignupPage.js
@@ -25,7 +25,7 @@ const SignupPage = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://wrightist.vercel.app/api/signup', {
+      const response = await fetch('https://wrightist-backend.vercel.app/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
