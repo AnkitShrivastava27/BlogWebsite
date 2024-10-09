c57cdd062262c42634ca5187e5ef11bbe6a51fb6
Ankit kr Shrivastava <ankitshrivastavaksa@gmail.com>
2024-10-09 16:41:00 +0530

    newcommit

--- api/index.js
+++ api/index.js
@@ -5,17 +5,13 @@ require('dotenv').config();
 
 const app = express();
 
-// CORS configuration (temporarily allowing all origins for testing)
-const corsOptions = {
-  origin: '*', // Temporarily allow all origins for testing
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200 // for legacy browsers (IE11, various SmartTVs) that choke on 204
-};
+// CORS configuration
 
-app.use(cors(corsOptions)); // Apply CORS settings
-
+app.use(cors({
+  origin: "https://wrightist.vercel.app/api/allpost"
+}));
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB URI
@@ -25,20 +21,6 @@ const mongoURI = process.env.MONGO_URI || "mongodb+srv://ankit:12ankit3@new.cq1e
 mongoose.connect(mongoURI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
-
-// Preflight request handling for OPTIONS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'wrightist.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  
-  next();
-});
 
 // Routes
 app.use('/', require('./routes/FirstPage'));
@@ -52,6 +34,8 @@ app.use('/api', require('./routes/likes'));
 app.use('/api', require('./routes/GetLikes'));
 app.use('/api', require('./routes/getpost'));
 app.use('/api', require('./routes/Delete'));
+
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
